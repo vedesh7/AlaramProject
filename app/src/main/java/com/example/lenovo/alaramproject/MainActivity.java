7599beaaaf68ff87b0b9875e64f947a291e8651c
vedesh7 <vedeshhegde@gmail.com>
2018-02-17 19:25:49 +0530

    Changes in MainActivity added one more java file called testClass

--- app/src/main/java/com/example/lenovo/alaramproject/MainActivity.java
+++ app/src/main/java/com/example/lenovo/alaramproject/MainActivity.java
@@ -37,10 +37,14 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        /*
+        Handle action bar item clicks here. The action bar will
+        automatically handle clicks on the Home/Up button, so long
+        as you specify a parent activity in AndroidManifest.xml.
+        as you specify a parent activity in Android main activity
+        */
+        int id;
+        id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
---
+++ app/src/main/java/com/example/lenovo/alaramproject/testClass.java
@@ -0,0 +1,8 @@
+package com.example.lenovo.alaramproject;
+
+/**
+ * Created by Lenovo on 17-02-2018.
+ */
+
+public class testClass {
+}
